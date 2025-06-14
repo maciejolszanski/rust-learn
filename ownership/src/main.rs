2cbbf60a6122fababab400785fa11818c362fc5e
Maciej Olszański <olszanskimaciej@wp.pl>
2025-06-14 18:02:24 +0200

    ownership

--- ownership/src/main.rs
+++ ownership/src/main.rs
@@ -58,4 +58,91 @@ fn main() {
     message.push_str(" is home");
     println!("message is {message}");
 
+    /*
+    Heap is big, but not infinite, so we have to clean up allocated memory that is no longer needed
+    This can be managed by programmed in C/C++, or by garbage collector in Java, Python, C#, Ruby, Go
+
+    Rust is using ownership for that, so variables are responsible for freeing their own resources according to the set of rules,
+    that can be checked by the compiler:
+      1. Every value is "owned" by one, and only one, variable at a time
+      2. When the owning variable goes out of scope, the value is dropped
+
+    Advantages:
+      - safe - it protects from the bugs like "invalid access"
+      - efficient - compiler can establish when the variable will be needed, no garbage collector running in the background
+
+    Disadvantages:
+      - requires understanding of ownership
+
+    */
+
+    let outer_planet: String;
+    {
+        let mut inner_planet = String::from("Mercury");
+        println!("inner_planet is {inner_planet}");
+        // new variable in a stack is pointing to the same heap memory (sth like shallow clone),
+        // but the previous pointer is invalidated, the ownership was moved
+        outer_planet = inner_planet;
+        // println!("inner_planet is {inner_planet}");  // will fail here
+        // if we want to copy the variable, we can use .clone() (like deep clone) - it'll duplicate data in heap, operation on one variable will not affect the other
+    }
+    println!("outer_planet is {outer_planet}");
+
+    // But Rust behaves differently for variables that are stored in Stack
+    // Data types with a known size, will implement the trait that allows them to be copies rather than moved like Strings
+    // Copying occuers implicitly, cloninig must be done explicitly
+    let outer_planet: i32;
+    {
+        let mut inner_planet = 1;
+        println!("inner_planet is {inner_planet}"); //1
+        outer_planet = inner_planet;
+        inner_planet += 1;
+        println!("inner_planet is {inner_planet}"); // 2
+    }
+    println!("outer_planet is {outer_planet}"); // 1
+
+    // Transfering ownership
+
+    // since the rocket_fuel is kept in Stack, when passing it as an argument the value is copied, 
+    // so there's new variable propellant in the Stack
+    let rocket_fuel = 1;
+    process_fuel(rocket_fuel);
+    println!("rocket_fuel is {rocket_fuel}");
+
+    /* 
+    When passing string as argument, the propellant is a new pointer to the same heap memory
+    so it takes ownership of it, but that means we are transfering ownerhip from rocket_fuel variable to the propellent variable
+    when the function is exited, the memory is disallocated, because the propellent variable is no longer needed
+    rocket_fuel variable cannot be used, because it no longer ownes the String
+    */
+    let rocket_fuel = String::from("RP-1");
+    process_fuel_2(rocket_fuel);
+    // println!("rocket_fuel is {rocket_fuel}"); // will fail
+
+
+    // first solution is to .clone() the variable
+    let rocket_fuel = String::from("RP-1");
+    process_fuel_2(rocket_fuel.clone());
+    println!("rocket_fuel is {rocket_fuel}");
+
+    // second solution is to pass the ownership back to the rocket_fuel variable
+    // the rocket_fuel returned by function doesn't have to have the same value
+    let rocket_fuel = String::from("RP-1");
+    let rocket_fuel = process_fuel_3(rocket_fuel);
+    println!("rocket_fuel is {rocket_fuel}");
+}
+
+fn process_fuel(mut propellant: i32) {
+    propellant += 1;
+    println!("rocessing propellant {propellant}...");
+}
+
+fn process_fuel_2(propellant: String) {
+    println!("rocessing propellant {propellant}...");
+}
+
+fn process_fuel_3(propellant: String) -> String {
+    println!("rocessing propellant {propellant}...");
+    let new_fuel = String::from("LNG");
+    new_fuel
 }
